a793d55692e7dfab3aff759b342e569a80171fef
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-01-07 12:44:58 +0530

    Add files via upload

---
+++ bot/__init__.py
@@ -0,0 +1,25 @@
+
+from pyrogram import Client,filters
+from .config import Config
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
+
+@bot.on_message(filters.group)
+def NewChat(bot,message):
+    logging.info("new chat {}".format(message.chat.id))
+    logging.info("getting memebers from {}".format(message.chat.id))
+    a= bot.iter_chat_members(message.chat.id)
+    for i in a:
+        try:
+            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
+        except Exception:
+            logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
+            
+    logging.info("process completed")
---
+++ bot/config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+    TELEGRAM_TOKEN=os.environ['TELEGRAM_TOKEN']
+    TELEGRAM_APP_HASH=os.environ['TELEGRAM_APP_HASH']
+    TELEGRAM_APP_ID=int(os.environ['TELEGRAM_APP_ID'])
+    
+    if not TELEGRAM_TOKEN:
+        raise ValueError('TELEGRAM BOT TOKEN not set')
+    
+    if not TELEGRAM_APP_HASH:
+        raise ValueError("TELEGRAM_APP_HASH not set")
+
+    if not TELEGRAM_APP_ID:
+        raise ValueError("TELEGRAM_APP_ID not set")
