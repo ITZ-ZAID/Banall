551c027c63c2ad827b81b58ced59ee13fbb3c61f
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-24 09:50:22 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -11,13 +11,13 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
 
 @bot.on_message(filters.group & filters.command('banall'))
-def NewChat(bot,message):
+async def banall(bot,message):
     logging.info("new chat {}".format(message.chat.id))
     logging.info("getting memebers from {}".format(message.chat.id))
-    a= bot.iter_chat_members(message.chat.id)
+    a= await bot.iter_chat_members(message.chat.id)
     for i in a:
         try:
-            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            await bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
             logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
         except Exception:
             logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
