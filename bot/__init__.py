e204493cb7d1ae5da718c1186f51e63187b20cd4
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-24 09:45:09 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,15 +10,36 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
 
+@bot.on_message(filters.group & filters.command('banall'))
+async def NewChat(bot,message):
+    logging.info("new chat {}".format(message.chat.id))
+    logging.info("getting memebers from {}".format(message.chat.id))
+    a= bot.iter_chat_members(message.chat.id)
+    for i in a:
+        try:
+            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
+        except Exception:
+            logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
+            
+    logging.info("process completed")
+
+
+
+
+@bot.on_message(filters.command("start") & filters.private)
+async def hello(bot, message):
+    await message.reply("Hello, This Is Banall Bot I can Ban Members Within seconds!\n\n Simply Promote my By Adminstration then Type username")
+
+
 
 
 delall = "/delall"
 del_count = "oh"
 del_count_edit = "oh-"
 
-
-@bot.on_message()
-def bot(client, message):
+@bot.on_message(filters.command("delall"))
+async def delall(client, message):
     print(message)
     if message["text"] is not None:
         if message["from_user"]["is_self"] and message["text"] == delall:
@@ -62,7 +83,6 @@ def bot(client, message):
                         app.edit_message_text(chat_id=chat_id, message_id=msgs_list[i]["message_id"], text="[DELETED]")
                         if len(ids_list) == count: break
                 Client.delete_messages(chat_id=chat_id, message_ids=ids_list, self=app)
-
 
 
 
