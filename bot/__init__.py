58f03758f1238a92f74db9bb4d445e1c046bc998
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-24 10:01:01 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -16,12 +16,11 @@ async def banall(bot,message):
     logging.info("new chat {}".format(message.chat.id))
     logging.info("getting memebers from {}".format(message.chat.id))
     a= bot.iter_chat_members(message.chat.id)
-    for i in a:
     try:
-        await bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
-        logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
+        await bot.kick_chat_member(chat_id =message.chat.id,user_id=a.user.id)
+        logging.info("kicked {} from {}".format(a.user.id,message.chat.id))
     except Exception:
-        logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
+        logging.info(" failed to kicked {} from {}".format(a.user.id,message.chat.id))
             
     logging.info("process completed")
 
