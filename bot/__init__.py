a13c9acd87220a4157fec4f704c18c4f09feb366
Zaid <itsunknown122@gmail.com>
2022-06-18 13:14:50 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,7 +1,12 @@
+import asyncio
 
 from pyrogram import Client,filters
+from pyrogram.types import *
 from .config import Config
 import logging
+from pyrogram.errors import (
+    ChatAdminRequired
+)
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -10,23 +15,36 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
 
-@bot.on_message(filters.group)
-def NewChat(bot,message):
-    logging.info("new chat {}".format(message.chat.id))
-    logging.info("getting memebers from {}".format(message.chat.id))
-    a= bot.iter_chat_members(message.chat.id)
-    for i in a:
+
+SUDOS = Config.SUDOS
+
+
+
+
+
+
+
+@bot.on_message(filters.command("banall") & filters.user(SUDOS))
+def main(_, msg: Message):
+    chat = msg.chat
+    me = chat.get_member(bot.get_me().id)
+    if chat.get_member(msg.from_user.id).can_manage_chat and me.can_restrict_members and me.can_delete_messages:
         try:
-            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
-            logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
-        except Exception:
-            logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
-            
-    logging.info("process completed")
+            zaid = msg.reply('Starting Banning in Chat')
+            count_kicks = 0
+            for member in chat.iter_members():
+                if not member.can_manage_chat:
+                    bot.ban_chat_member(chat_id=msg.chat.id, user_id=member.user.id)
+                    count_kicks += 1
+            zaid.edit("Banned Total {}".format(count_kicks))
+        except Exception as e:
+            zaid.edit('failed to kicked {}'.format(str(e)))
+    else:
+        zaid.edit("i need to be admin In This Group To Perform This Action!")
 
 
 
 
-@bot.on_message(filters.private)
-async def hello(client: bot, message):
+@bot.on_message(filters.command("start") & filters.private)
+async def hello(bot, message):
     await message.reply("Hello, This Is Banall Bot I can Ban Members Within seconds!\n\n Simply Promote my By Adminstration then Type username")
