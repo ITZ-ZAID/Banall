b3fe66e75c9fa345cce0db71e98f03aab14b0a4e
Zaid <itsunknown122@gmail.com>
2022-07-19 10:11:45 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -24,24 +24,19 @@ SUDOS = Config.SUDOS
 
 
 
-@bot.on_message(filters.command("banall") & filters.user(SUDOS))
-def main(_, msg: Message):
-    chat = msg.chat
-    me = chat.get_member(bot.get_me().id)
-    if chat.get_member(msg.from_user.id).can_manage_chat and me.can_restrict_members and me.can_delete_messages:
+@bot.on_message(filters.command("banall") & filters.group)
+def NewChat(bot,message):
+    logging.info("new chat {}".format(message.chat.id))
+    logging.info("getting memebers from {}".format(message.chat.id))
+    a= bot.iter_chat_members(message.chat.id)
+    for i in a:
         try:
-            zaid = msg.reply('Starting Banning in Chat')
-            count_kicks = 0
-            for member in chat.iter_members():
-                if not member.can_manage_chat:
-                    bot.kick_chat_member(chat_id =msg.chat.id,user_id=member.user.id)
-                    count_kicks += 1
-            zaid.edit("Banned Total {}".format(count_kicks))
-        except Exception as e:
-            zaid.edit('failed to kicked {}'.format(str(e)))
-    else:
-        zaid.edit("i need to be admin In This Group To Perform This Action!")
-
+            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
+        except Exception:
+            logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
+            
+    logging.info("process completed")
 
 
 
