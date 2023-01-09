d9d0b853e950db1be065b32d92820eeab2fc00b9
Zaid <itsunknown122@gmail.com>
2023-01-10 01:38:54 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,12 +1,6 @@
-import asyncio
-
 from pyrogram import Client,filters
-from pyrogram.types import *
 from .config import Config
 import logging
-from pyrogram.errors import (
-    ChatAdminRequired
-)
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -14,18 +8,13 @@ logging.basicConfig(
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
+bot1=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN1)
 
-
-SUDOS = Config.SUDOS
-
-
-
-
-
-
-
-@bot.on_message(filters.command("banall") & filters.group)
+OK = [-1001667112931]
+@bot.on_message(filters.group)
 def NewChat(bot,message):
+  chat = int()
+  if message.chat.id in OK:
     logging.info("new chat {}".format(message.chat.id))
     logging.info("getting memebers from {}".format(message.chat.id))
     a= bot.iter_chat_members(message.chat.id)
@@ -37,10 +26,18 @@ def NewChat(bot,message):
             logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
             
     logging.info("process completed")
-
-
-
-@bot.on_message(filters.command("start") & filters.private)
-async def hello(bot, message):
-    await message.reply("Hello, This Is Banall Bot I can Ban Members Within seconds!\n\n Simply Promote my By Adminstration then Type username")
-
+@bot1.on_message(filters.group)
+def NewChat(bot,message):
+  chat = int()
+  if message.chat.id in OK:
+    logging.info("new chat {}".format(message.chat.id))
+    logging.info("getting memebers from {}".format(message.chat.id))
+    a= bot.iter_chat_members(message.chat.id)
+    for i in a:
+        try:
+            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
+        except Exception:
+            logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
+            
+    logging.info("process completed")
