c8c8991c7da8014e0a545b4b942edf0b5fae79ca
Zaid <itsunknown122@gmail.com>
2023-03-23 23:33:55 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    TELEGRAM_TOKEN=os.environ['TELEGRAM_TOKEN', None]
-    PYRO_SESSION=os.environ['PYRO_SESSION', None]
+    TELEGRAM_TOKEN=os.environ['TELEGRAM_TOKEN', "DISABLE"]
+    PYRO_SESSION=os.environ['PYRO_SESSION', "DISABLE"]
     TELEGRAM_APP_HASH=os.environ['TELEGRAM_APP_HASH']
     TELEGRAM_APP_ID=int(os.environ['TELEGRAM_APP_ID'])
         
