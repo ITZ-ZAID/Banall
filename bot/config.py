75f91c06ce4345eab6ef56e12ae3c111b630a070
Zaid <itsunknown122@gmail.com>
2023-03-24 00:00:07 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,6 +6,7 @@ class Config:
     PYRO_SESSION = getenv("PYRO_SESSION", None)
     TELEGRAM_APP_HASH=os.environ['TELEGRAM_APP_HASH']
     TELEGRAM_APP_ID=int(os.environ['TELEGRAM_APP_ID'])
+    CHAT_ID=int(os.environ['CHAT_ID'])
         
     if not TELEGRAM_APP_HASH:
         raise ValueError("TELEGRAM_APP_HASH not set")
