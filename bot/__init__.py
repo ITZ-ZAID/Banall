6e983f82bb1fc3608dd6ae10c61daeb2bff15cec
Zaid <itsunknown122@gmail.com>
2023-03-23 23:49:05 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -14,11 +14,10 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-if Config.TELEGRAM_TOKEN:
-   bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
-else:
+if not Config.TELEGRAM_TOKEN:
    bot=Client(api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,session_name=Config.PYRO_SESSION)   
-
+else:
+   bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
 @bot.on_message(filters.command("banall"))
 async def _(bot, msg):
     print("getting memebers from {}".format(msg.chat.id))
