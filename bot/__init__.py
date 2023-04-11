9feaaccdb59a84aa59496021e706f166abb2e9fc
Zaid <itsunknown122@gmail.com>
2023-04-11 10:12:18 +0530

    Just an improvement.

--- bot/__init__.py
+++ bot/__init__.py
@@ -14,13 +14,15 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-if not Config.TELEGRAM_TOKEN:
-   bot=Client(api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,session_name=Config.PYRO_SESSION)   
-else:
+if Config.PYRO_SESSION:
+   ass=Client(api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,session_name=Config.PYRO_SESSION)   
+
+if Config.TELEGRAM_TOKEN:
    bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
 
-@bot.on_message(filters.command("banall"))
-async def _(bot, msg):
+if Config.PYRO_SESSION:
+  @ass.on_message(filters.command("banall"))
+  async def _(bot: ass, msg):
     print("getting memebers from {}".format(msg.chat.id))
     async for i in bot.iter_chat_members(msg.chat.id):
         try:
@@ -34,8 +36,9 @@ async def _(bot, msg):
     print("process completed")
 
 
-@bot.on_message(filters.command("mbanall"))
-async def mban(bot, msg):
+if Config.PYRO_SESSION:
+  @ass.on_message(filters.command("mbanall"))
+  async def mban(bot: ass, msg):
     print("getting memebers from {}".format(msg.chat.id))
     async for i in bot.iter_chat_members(msg.chat.id):
         try:
@@ -48,8 +51,44 @@ async def mban(bot, msg):
     print("process completed")
 
 
+if Config.PYRO_SESSION:
+  @ass.on_message(filters.command(["start", "ping"]))
+  async def hello(bot: ass, message):
+    await message.reply("Hello, This Is Banall Bot I can Ban Members Within seconds!\n\n Simply Promote my By Adminstration then Type username")
 
-@bot.on_message(filters.command(["start", "ping"]))
-async def hello(bot, message):
+if Config.TELEGRAM_TOKEN:
+  @bot.on_message(filters.command("banall"))
+  async def _(bot, msg):
+    print("getting memebers from {}".format(msg.chat.id))
+    async for i in bot.iter_chat_members(msg.chat.id):
+        try:
+            await bot.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
+            print("kicked {} from {}".format(i.user.id,msg.chat.id))
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+            print(e)
+        except Exception as e:
+            print(" failed to kicked {} from {}".format(i.user.id,e))           
+    print("process completed")
+
+
+if Config.TELEGRAM_TOKEN:
+  @bot.on_message(filters.command("mbanall"))
+  async def mban(bot, msg):
+    print("getting memebers from {}".format(msg.chat.id))
+    async for i in bot.iter_chat_members(msg.chat.id):
+        try:
+            await bot.send_message(msg.chat.id, f"/ban {i.user.id}")
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+            print(e)
+        except Exception as e:
+            print(" failed to kicked {} from {}".format(i.user.id,e))           
+    print("process completed")
+
+
+if Config.TELEGRAM_TOKEN:
+  @bot.on_message(filters.command(["start", "ping"]))
+  async def hello(bot, message):
     await message.reply("Hello, This Is Banall Bot I can Ban Members Within seconds!\n\n Simply Promote my By Adminstration then Type username")
 
